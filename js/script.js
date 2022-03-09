9d75af097bfb9b649bd3ddafa975ca4690ae6b72
Christian Podda <poddachristian94@gmail.com>
2022-03-09 22:44:08 +0100

    incompleto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 // console check
-console.log('JS OK!')
-const user = prompt('Benvenuto, utente. Inserisci la tua e-mail per accedere al sito')
+console.log("JS OK!")
+
+// indirizzi registrati
+const email = ["s.bianchi@yao.it", "m.rossi@gail.it", "g.verdi@hmail"];
+var user = email
+
+// messaggio di benvenuto
+var user = prompt("Benvenuto, utente sconosciuto. Inserisci la tua e-mail per accedere al sito")
 console.log(user)
 
 // email convalidate
-const email = ['s.bianchi@yao.it',]
+if (user = email) {
+    alert("Bentornato, utente registrato");
+}
+else {
+    alert("Errore durante l'accesso, ricarica la pagina e riprova");
+}
