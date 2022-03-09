13db9b9b0977871b82e07c99fdd3e2fb68c4febf
Christian Podda <poddachristian94@gmail.com>
2022-03-09 17:30:44 +0100

    Init + start

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// console check
+console.log('JS OK!')
+const user = prompt('Benvenuto, utente. Inserisci la tua e-mail per accedere al sito')
+console.log(user)
+
+// email convalidate
+const email = ['s.bianchi@yao.it',]
