0431cafa720bc7301c7438068f9b81dcd0ecd610
Christian Podda <poddachristian94@gmail.com>
2022-03-10 19:38:07 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@ console.log("JS OK!")
 
 // indirizzi registrati
 const email = ["s.bianchi@yao.it", "m.rossi@gail.it", "g.verdi@hmail"];
-var user = email
 
-// messaggio di benvenuto
-var user = prompt("Benvenuto, utente sconosciuto. Inserisci la tua e-mail per accedere al sito")
+// messaggio di benvenuto + prompt
+const user = prompt("Benvenuto, utente sconosciuto. Inserisci la tua e-mail per accedere al sito")
 console.log(user)
 
 // email convalidate
-if (user = email) {
-    alert("Bentornato, utente registrato");
-}
-else {
-    alert("Errore durante l'accesso, ricarica la pagina e riprova");
+if (user) {
+    for (let i = 0; i < email.length; i++) {
+        const registered = email[i];
+        if (user === registered) {
+            alert("Bentornato, utente registrato");
+        } else {
+            alert("Indirizzo e-mail non trovato")
+        }
+    }
 }
